c33b40f9efc535efa093a68767e3f6ad26230d93
fr3dr33k <jcb_frd1991@yahoo.com>
2020-12-14 10:42:47 +0100

    update emailVerifier

--- assets/emailVerifier/emailVerif.php
+++ assets/emailVerifier/emailVerif.php
@@ -1,35 +1,39 @@
 <?php
 
-$curl = curl_init();
+	use App\Controllers\ControllerUserActions;
 
-if(isset($_POST["email"])){
-	$email = $_POST['email'];
-}else{
-	$email = "jacky@yahoo.fr";
-}
+	$curl = curl_init();
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://email-checker.p.rapidapi.com/verify/v1?email=".$email,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: email-checker.p.rapidapi.com",
-		"x-rapidapi-key: 8f95f8118amshed510089362e44ap11181fjsn596a754a51ca"
-	],
-]);
+	if(isset($_POST["email"])){
+		$email = $_POST['email'];
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+	}else{
+		$email = "jacky@yahoo.fr";
+	}
 
-curl_close($curl);
+	curl_setopt_array($curl, [
+		CURLOPT_URL => "https://email-checker.p.rapidapi.com/verify/v1?email=".$email,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => [
+			"x-rapidapi-host: email-checker.p.rapidapi.com",
+			"x-rapidapi-key: 8f95f8118amshed510089362e44ap11181fjsn596a754a51ca"
+		],
+	]);
 
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
-}
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+		$response = json_decode($response, TRUE);
+		print_r($response);
+	}
