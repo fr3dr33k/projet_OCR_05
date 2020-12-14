5910697cefd47e9e15b932ad7fc2f58d76ac7ef4
fr3dr33k <jcb_frd1991@yahoo.com>
2020-12-14 10:43:08 +0100

    update navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,21 +1,38 @@
- Navbaruss =  {
+ Navbaruss = {
 
-   init : function(param){
-       switch(param){
+    init : function(param){
+
+        switch(param){
             case "email":
-                if($("#form").is(":visible")){
-                    $("#form").hide()
+                $(".close span").on("click", function(){
+                    $(".bloc_send_mail").hide()
+                })
+                if($(".bloc_send_mail").is(":visible") || $(".bloc_phone").is(":visible")){          
+                    $(".bloc_phone").hide()
+                    $(".bloc_send_mail").toggle()
+                    $(".bloc_button").focus({preventScroll:true})
                 }else{
-                    $("#form").toggle()
+                    $(".bloc_send_mail").toggle()
+                    $(".bloc_button").focus({preventScroll:true})
                 }
-                break;
-            case "téléphone":
-                
             break;
+            case "téléphone":
+                $(".close span").on("click", function(){
+                    $(".bloc_phone").hide()
+                })
+                if($(".bloc_send_mail").is(":visible") || $(".bloc_phone").is(":visible")){
+                    $(".bloc_send_mail").hide() 
+                    $(".bloc_phone").toggle()  
+                    $(".bloc_button").focus({preventScroll:true})
+                }else{
+                    $(".bloc_phone").toggle()
+                    $(".bloc_button").focus({preventScroll:true})
+                }
+            break;
+        }
 
-       }
-   }
+    }
 
 }
 
-$('.carousel').carousel()
+Navbaruss.init()
